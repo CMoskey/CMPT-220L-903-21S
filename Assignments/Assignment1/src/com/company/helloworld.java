9ea0b983d7113503a3628a78099339081c85d0bc
CMoskey <cameron.moskey@gmail.com>
2021-03-08 21:29:06 -0500

    Assignment3

---
+++ Assignments/Assignment1/src/com/company/helloworld.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class helloworld {
+
+    public static void main(String[] args) {
+	System.out.println("This one is for the boys with the booming system\n" +
+            "Top down, AC with the cooler system\n" +
+            "When he come up in the club, he be blazin' up\n" +
+            "Got stacks on deck like he savin' up\n" +
+            "And he ill, he real, he might gotta deal\n" +
+            "He pop bottles and he got the right kind of build\n" +
+            "He cold, he dope, he might sell Coke\n" +
+            "He always in the air, but he never fly coach");
+    }
+}
